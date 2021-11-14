380ef42b1f714d0a4d2f815c3f534a2d5e079752
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-11-14 11:10:46 +0800

    -fixed codinng error

--- Program3_Life_Stages.py
+++ Program3_Life_Stages.py
@@ -3,8 +3,8 @@ if age >= 0 and age <=12:
    print("kid")
 elif age >= 13 and age <=17:
    print("teen")
-elif age := 18:
+elif age == 18:
    print("debut")
 elif age >= 19:
-   print("kid")  
+   print("Adult")  
 print("done!")
