ebf2ed4c6315f3cc53829116eeeea4a5d1c926ef
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-11-14 10:02:54 +0800

    added base code for program 3, no def function

---
+++ Program3_Life_Stages.py
@@ -0,0 +1,10 @@
+age=int(input("Type your age:"))
+if age >= 0 and age <=12:
+   print("kid")
+elif age >= 13 and age <=17:
+   print("teen")
+elif age := 18:
+   print("debut")
+elif age >= 19:
+   print("kid")  
+print("done!")
