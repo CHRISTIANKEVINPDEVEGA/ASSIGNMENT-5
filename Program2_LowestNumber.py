6494f63de477fccc5c2f83a1bd9816938fba0569
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-11-18 17:53:53 +0800

    -def function was used, -added some text, -single letter variables ware removed and replaced by words

--- Program2_LowestNumber.py
+++ Program2_LowestNumber.py
@@ -1,19 +1,18 @@
-a = float(input("Give 1st number:"))
-b = float(input("Give 2nd number:"))
-c = float(input("Give 3rd number:"))
+def ask_4_number():
+ _FIRST_number = float(input("Give a 1st number:"))
+ _SECOND_number = float(input("Give a 2nd number:"))
+ _THIRD_number = float(input("Give a 3rd number:"))
+ return _FIRST_number, _SECOND_number, _THIRD_number
 
+def display( first_num_, second_num_, third_num_):
+ if first_num_<=second_num_ and first_num_<=third_num_:
+    print(f"Among the three given number, the lowest is {first_num_:.2f}.")
+ elif second_num_<=first_num_ and second_num_<=third_num_:
+    print(f"Among the three given number, the lowest is {second_num_:.2f}.")    
+ elif third_num_<=first_num_ and third_num_<=second_num_:
+    print(f"Among the three given number, the lowest is {third_num_:.2f}.") 
+ elif first_num_==second_num_ and second_num_==third_num_ and first_num_==third_num_:
+    print(f"Among the three given number, the lowest is {first_num_:.2f}.")
 
-if a<=b and a<=c:
-    print(f"{a:.2f}")
-elif b<=a and b<=c:
-    print(f"{b:.2f}")    
-elif c<=a and c<=b:
-    print(f"{c:.2f}") 
-elif a==b and b==c and a==c:
-    print(f"{a,b,c}")
-
-
-
-
-else:
-    print("andoy baho!")
+first_number, second_number, third_number = ask_4_number()
+display(first_number, second_number, third_number)
