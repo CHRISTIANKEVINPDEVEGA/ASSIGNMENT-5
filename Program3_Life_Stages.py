d112486aa2007627d327f7fcb50e930bf77f8ec8
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-11-18 17:41:14 +0800

    -def function was used, -added some extra text

--- Program3_Life_Stages.py
+++ Program3_Life_Stages.py
@@ -1,10 +1,18 @@
-age=int(input("Type your age:"))
-if age >= 0 and age <=12:
-   print("kid")
-elif age >= 13 and age <=17:
-   print("teen")
-elif age == 18:
-   print("debut")
-elif age >= 19:
-   print("Adult")  
+def ask_4_age():
+   age_input=int(input("Type your age:"))
+   return age_input
+
+def display(age):
+   if age >= 0 and age <=12:
+    print("life stage: Kid")
+   elif age >= 13 and age <=17:
+    print("life stage: Teen")
+   elif age == 18:
+    print("life stage: Debut")
+   elif age >= 19:
+    print("life stage: Adult")  
+
+ASK_4_AGE=ask_4_age()
+display(ASK_4_AGE)
+
 print("done!")
